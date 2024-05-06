ca4ce07f7ad0f68ac66aecc8e0eb472de4f320f6
dvnber10 <dvnber2001@gmail.com>
2024-05-06 08:40:56 -0500

    update 06 April, 08:20 AM, add oneUser

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import "../../styles/OneArticle.css"; // Archivo de estilos CSS
+import Navbar from "../Navbar";
+import { useParams, useNavigate } from "react-router-dom";
+//import articulos from "./articulosData";
+import Cookies from "universal-cookie";
+
+const OneArticle = () => {
+
+    /* Cookie */
+/* import Cookies from "universal-cookie"; */
+  const navigate = useNavigate();
+  const cookie = new Cookies();
+  const cook = cookie.get('id')
+  useEffect(() => {
+    if (!cook) {
+      navigate('/time-out') // Hay que crear la ruta time out que es el cierre de sesioón
+    }
+  })
+/* Cookie */
+const {data:result, isSuccess} = GetAllArticles(cook);
+const users = isSuccess && result.data;
+  const { id } = useParams(); // Obtener el valor del parámetro de la ruta
+
+  // Verificar si el ID es válido y obtener el artículo correspondiente
+  const indiceInicial = parseInt(id) - 1;
+  const [indiceArticulo, setIndiceArticulo] = useState(indiceInicial);
+
+  useEffect(() => {
+    // Actualizar la URL cuando cambia el índice del artículo
+    navigate(`/oneArticle/${indiceArticulo + 1}`);
+  }, [indiceArticulo, navigate]);
+
+  const handleNavigateToTexts = () => {
+    navigate("/AllUsers");
+  };
+
+  const mostrarSiguienteArticulo = () => {
+    setIndiceArticulo((indiceArticulo + 1) % users.length);
+  };
+
+  const mostrarArticuloAnterior = () => {
+    const nuevoIndice =
+      indiceArticulo - 1 < 0 ? users.length - 1 : indiceArticulo - 1;
+    setIndiceArticulo(nuevoIndice);
+  };
+
+  const user = users[indiceArticulo];
+  console.log(user)
+  return (
+    <div className="rp-cont">
+      <Navbar />
+      <div id="article">
+        <div id="div-titulo">
+          <h2 id="titulo">{isSuccess && user.userName}</h2>
+        </div>
+        <img
+          src={isSuccess && user.Telefono}
+          alt={isSuccess && user.Correo}
+          id="imagen-articulo"
+        />
+        <h2 id="date">{isSuccess && user.idUser}</h2>
+        <a
+          href={isSuccess && articulo.userName}
+          target="_blank"
+          rel="noopener noreferrer"
+          id="boton-ver"
+        >
+          Ver
+        </a>
+      </div>
+      <div id="controles">
+        <button onClick={mostrarArticuloAnterior}>Anterior</button>
+        <div id="num" onClick={handleNavigateToTexts}>
+          <p>
+            {indiceArticulo + 1}/{users.length}
+            <img
+              src="../../src/image/icon/list.svg"
+              alt=""
+              id="list-oneArticle"
+            />
+          </p>
+        </div>
+        <button onClick={mostrarSiguienteArticulo}>Siguiente</button>
+      </div>
+    </div>
+  );
+};
+
+export default OneArticle;
